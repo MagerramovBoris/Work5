11b9322dfc318e85d87a9ae8d54d3f2edaa64577
MagerramovBoris <magerramov.boris.s@gmail.com>
2021-01-26 20:33:10 +0700

    Пункт 7. Добавил api.

---
+++ app/src/main/java/com/example/work5/api/FlickrAPI.java
@@ -0,0 +1,22 @@
+package com.example.work5.api;
+
+import com.example.Work5_beta.model.FlickrPhotos;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FlickrAPI {
+    // Получить фото
+    @GET("services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1")
+    Call<FlickrPhotos>
+    getRecent(@Query("api_key") String api_key);
+
+    // Получить фото по запросу
+    @GET("services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1")
+    Call<FlickrPhotos>
+    getSearchPhotos(
+            @Query("api_key") String api_key,
+            @Query("text") String keyWord
+    );
+}
